7aea42a2e0b50e18a5f066d1ca7110446325c1ea
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2020-06-09 19:16:31 +0330

    'commit'

---
+++ precache-manifest.b8afc86fc798bc952dceea84b751ecfd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3b3eee6c545952306d578979599b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93f2041bf9ab9848060d",
+    "url": "/static/css/2.c3304c5a.chunk.css"
+  },
+  {
+    "revision": "eb212ac84665099e581f",
+    "url": "/static/css/main.f299684d.chunk.css"
+  },
+  {
+    "revision": "93f2041bf9ab9848060d",
+    "url": "/static/js/2.504c0dea.chunk.js"
+  },
+  {
+    "revision": "153f93a94a1d33b36748190806b61d28",
+    "url": "/static/js/2.504c0dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb212ac84665099e581f",
+    "url": "/static/js/main.92db181e.chunk.js"
+  },
+  {
+    "revision": "046e4098ab3bba465d28",
+    "url": "/static/js/runtime-main.d72e5d23.js"
+  },
+  {
+    "revision": "a656dea8c8adfa6a678eadfd4163cc6c",
+    "url": "/static/media/about.a656dea8.png"
+  },
+  {
+    "revision": "137b1855e654ff81cb29a6365b054af3",
+    "url": "/static/media/dark.137b1855.png"
+  },
+  {
+    "revision": "49d697a88c2e80542bc709b3b6c2650c",
+    "url": "/static/media/fat.49d697a8.png"
+  },
+  {
+    "revision": "6f84aea38241d59ca367cf921048d3c8",
+    "url": "/static/media/iran.6f84aea3.png"
+  },
+  {
+    "revision": "95ab1a2d7b3509066ad7f019d72804b7",
+    "url": "/static/media/logo.95ab1a2d.png"
+  },
+  {
+    "revision": "4e62f00d676ab41f895b80a2bc99a37d",
+    "url": "/static/media/normal.4e62f00d.png"
+  },
+  {
+    "revision": "9e877dbdcb318b360bcc64653d82dd78",
+    "url": "/static/media/over.9e877dbd.png"
+  },
+  {
+    "revision": "e21327386c16b1635d47a8a5deff0dec",
+    "url": "/static/media/uk.e2132738.png"
+  },
+  {
+    "revision": "a892557c9d079ab865c6047732eca59e",
+    "url": "/static/media/under.a892557c.png"
+  },
+  {
+    "revision": "2c4caade5d4a4162d911c48c795b36f3",
+    "url": "/static/media/us.2c4caade.png"
+  }
+]);
